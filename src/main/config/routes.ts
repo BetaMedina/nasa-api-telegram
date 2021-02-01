30c800627750302a0553d6e828b827b196bfecc7
MedinaAdrade <joaopedromedinadeandrade@gmail.com>
2021-02-01 00:07:35 -0300

    teste

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,14 @@
 import { Express, Router } from 'express'
 import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import { createImportSpecifier } from 'typescript'
 
 export default (app:Express):void => {
   const router = Router()
   app.use(router)
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**/**routes.ts').map(async file => {
-    (await import(`../../../${file}`)).default(router)
+  // eslint-disable-next-line node/no-path-concat
+  readdirSync(`${__dirname}/../routes`).map(async file => {
+    (await import(`../routes/${file}/${file}-routes`)).default(router)
   })
 }
