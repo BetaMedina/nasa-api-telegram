fca1ac72de7a27e9dc2a6bb73faee98abafbadc6
MedinaAdrade <joaopedromedinadeandrade@gmail.com>
2021-02-01 01:21:13 -0300

    format text

--- src/presentation/controller/nasa/feed-today/enum/payload.enum.ts
+++ src/presentation/controller/nasa/feed-today/enum/payload.enum.ts
@@ -1,5 +1,5 @@
 export const TELEGRAM_ENUM = {
   CLIENT_MESSAGE: 'Algum asteroide oferece perigo ao planeta Terra na data de hoje?',
   EMPTY_MESSAGE: 'Não, fique tranquilo ;-)',
-  ERROR_MESSAGE: 'Desculpe, não entendi sua pergunta... caso queira ver dados de algum meteoro, escreva:Algum asteroide oferece perigo ao planeta Terra na data de hoje?'
+  ERROR_MESSAGE: 'Desculpe, não entendi sua pergunta...\ncaso queira ver dados de algum meteoro, escreva:Algum asteroide oferece perigo ao planeta Terra na data de hoje?'
 }
